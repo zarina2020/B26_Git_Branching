c13307e4ade09ef67ba335d2924084f9df0c5fb9
zarina <zarina.asxodjaeva@gmail.com>
2023-05-10 13:35:20 -0500

    updated my massage

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -5,5 +5,10 @@ public class TC002 {
     public static void main(String[] args) {
         // Today is weather blurry
         // Amina with grandmother goto outside
+
+
+        // creating a report
+        // screenshot in failure
+
     }
 }
